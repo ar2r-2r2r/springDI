4cf27362073b11b1322e2b6cb0515d9496a06cd8
ar2r-java <roooob66666666666@gmail.com>
2022-06-19 02:31:56 +0300

    v4InitDestroy

--- src/main/java/springcourse/ClassicalMusic.java
+++ src/main/java/springcourse/ClassicalMusic.java
@@ -1,6 +1,14 @@
 package springcourse;
 
 public class ClassicalMusic implements Music{
+    public void myInit(){
+        System.out.println("Initialization is correct!");
+    }
+
+    public void myDestroy() {
+        System.out.println("Destroy is correct!");
+    }
+
     @Override
     public  String getSong(){
         return "Hungarian Rhapsody";
