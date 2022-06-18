00afe947462cb26ed941773ce579760823e6f837
ar2r-java <roooob66666666666@gmail.com>
2022-06-19 02:07:30 +0300

    v4SingletonAsDefault

--- src/main/java/springcourse/TestSpring.java
+++ src/main/java/springcourse/TestSpring.java
@@ -8,10 +8,19 @@ public class TestSpring {
                 "applicationContext.xml"
                 );
 
-        MusicPlayer musicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer firstMusicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
 
+        boolean comparison= firstMusicPlayer==secondMusicPlayer;
+        System.out.println(comparison);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+        firstMusicPlayer.playMusicList();
+        secondMusicPlayer.playMusicList();
 
-        musicPlayer.playMusicList();
+        firstMusicPlayer.setVolume(70);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
         context.close();
     }
 }
