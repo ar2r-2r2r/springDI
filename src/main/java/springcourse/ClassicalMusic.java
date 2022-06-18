875a2510be15918396b8bbbe5c2b95d6a7522def
ar2r-java <roooob66666666666@gmail.com>
2022-06-19 00:44:51 +0300

    Initial commit

---
+++ src/main/java/springcourse/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package springcourse;
+
+public class ClassicalMusic implements Music{
+    @Override
+    public  String getSong(){
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/java/springcourse/MusicPlayer.java
@@ -0,0 +1,16 @@
+package springcourse;
+
+public class MusicPlayer {
+    private Music music;
+
+    //Invertional of control
+    public MusicPlayer(Music music){
+    this.music=music;
+    }
+
+    public void playMusic(){
+        System.out.println("Playing: "+music.getSong() );
+    }
+
+
+}
